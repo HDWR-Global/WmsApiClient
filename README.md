098e64ab5bfe7a2ed89b0ea64bea0b7f1c0a249d
Szymon Jakóbczak <hdwrdev@gmail.com>
2024-05-16 11:27:04 +0200

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Poniższe biblioteki umożliwiają połączenie z API [systemu magazynowego](https://weaverwms.com/pl) Weaver WMS.
+
+### Instalacja bibliotek
+
+1. Należy pobrać zbiór paczek Nuget z strony [Weaver WMS](https://weaverwms.com/downloads/wmsall), np. najnowszą na ten moment wersję [apiClientNuGet_4.59.0.0.zip](http://https://weaverwms.com/downloads/wmsall/4.59.0.0/apiClientNuGet_4.59.0.0.zip)
+2. Rozpakować i dodać folder do źródeł menedżera pakietów Nuget
+3. Zainstalować pakiet Weaver.Wms.Api.Client
---
+++ WmsApiClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using Weaver.Wms.Api.Client.Interfaces;
+
+namespace WmsApiClient
+{
+    internal static class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new Weaver.Wms.Api.Client.WmsApiClient("http://localhost:43400", true);
+            await client.Login("admin", "admin");
+            Console.WriteLine("Materiały:");
+            int page = 0;
+            int pages = 1;
+            while (page < pages)
+            {
+                page++;
+                var materials = await client.CreateRequest<IMaterialClient>().List(new Weaver.Wms.ServiceInterfaces.MaterialService.MaterialFilterDto { Page = page });
+                pages = materials.Pages;
+                foreach (var material in materials.Items)
+                {
+                    Console.WriteLine($"{material.Reference} - {material.Name}");
+                }
+            }
+            await client.Logout();
+        }
+    }
+}
